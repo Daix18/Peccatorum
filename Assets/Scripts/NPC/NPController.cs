b828fc274d7de8340e9185e6a76938ffc68736ec
Daix18 <danizaguirre10@gmail.com>
2024-01-21 21:45:15 +0100

    Dialogue Completed
    Dialogo con diferentes NPCS y con el package de tint incluido, también incluye el que salgan las palabras por characteres.

--- Assets/Scripts/NPC/NPController.cs
+++ Assets/Scripts/NPC/NPController.cs
@@ -1,42 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NPController : MonoBehaviour
 {
-    public static NPController THIS;
-
-    public bool playerIsClose;
-
     // Start is called before the first frame update
     void Start()
     {
-
-    }
-
-    private void Awake()
-    {
-        THIS = this;
-
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerIsClose = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerIsClose = false;
-        }
     }
 }
---
+++ Assets/Scripts/UI/DialogueTrigger.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [Header("Visual Cue")]
+    [SerializeField] private GameObject visualCue;
+
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJson;
+    bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInRange = false;
+        visualCue.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInRange)
+        {
+            visualCue.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                DialogueController.GetInstance().EnterDialogueMode(inkJson);
+            }
+        }
+        else
+        {
+            visualCue.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
