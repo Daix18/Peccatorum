fa0f04d9310db4df4f4c6d25e05ffb345a90c05f
Daix18 <danizaguirre10@gmail.com>
2024-03-21 19:47:17 +0100

    Respawn Fixed

--- Assets/Scripts/Player/RespawnSystem.cs
+++ Assets/Scripts/Player/RespawnSystem.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class RespawnSystem : MonoBehaviour
 {
-    private Vector3 lastSpawnPoint; // Variable para almacenar la posición del último "spawn point"
+    private Vector2 lastSpawnPoint; // Variable para almacenar la posición del último "spawn point"
+
+    private void Start()
+    {
+        lastSpawnPoint = transform.position;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // Actualizar la posición del último "spawn point" solo si la vida del jugador es menor o igual a cero
-            AttackController attackController = other.GetComponent<AttackController>();
-            if (attackController != null && attackController.GetCurrentHealth() <= 0)
-            {
-                lastSpawnPoint = transform.position;
-            }
+            // Actualizar la posición del último "spawn point" al pasar por el spawn point
+            Debug.Log("Player ha pasado por el spawn point: " + transform.position);
+            lastSpawnPoint = transform.position;
         }
     }
 
-    public Vector3 GetLastSpawnPoint()
+    public Vector2 GetLastSpawnPoint()
     {
         return lastSpawnPoint;
-    }
+    }   
 }
